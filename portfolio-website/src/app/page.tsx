dce7941308e2d1af3380703f29c3f517087a2c62
Cesar Quesada <cquesada1611@gmail.com>
2024-11-16 11:33:31 -0400

    Added Twitter link and rewrote Header

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -1,27 +1,13 @@
-import Image from "next/image";
-import Link from "next/link";
-import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
-import { FaSquareInstagram } from "react-icons/fa6";
+import React from "react";
+import PageHeader from "@/components/Header/index";
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-center items-center min-h-screen bg-[#eedccd] text-[#3b580c] font-extrabold gap-6 ">
-      <div className="">
-      <Image src={"/logo.png"} alt={"Profile Image"} width={150} height={150}/>
-      </div>
+    <div className="flex justify-center items-center min-h-screen bg-[#eedccd] text-[#3b580c]">
+      {/* Centered Content */}
       
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl">Cesar A. Quesada</h1>
-        <h2 className="text-2xl font-Bold">Fullstack developer</h2>
-        {/* todo: add Bayamon google maps link, add map pin emoji to end */}
-        <p className="text-base text-gray-600"><span className="text-[#3b580c]">Bayamon,</span> Puerto Rico </p>
-        <div className="flex flex-row gap-2">
-          {/* todo: change text into the icons */}
-          <Link className="text-3xl hover:text-[#0077B5]" href={"/"}><FaLinkedin /></Link>
-          <Link className="text-3xl hover:text-[#333]" href={"/"}><FaGithubSquare/></Link>
-          <Link className="text-3xl hover:text-[#E4405F]" href={"/"}><FaSquareInstagram/></Link>
-        </div>
-      </div>
+        <PageHeader />
+      
     </div>
   );
 }
---
+++ portfolio-website/src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
+import { RiTwitterXFill } from "react-icons/ri";
+
+
+function PageHeader() {
+  return (
+    <div className="flex flex-col md:flex-row items-center md:items-start max-w-4xl md:space-x-6 md:space-y-0 space-y-10">
+      {/* Profile image */}
+        <Image src={"/logo.png"} alt={"Profile Image"} width={150} height={150} />
+
+        <div className="max-w-72">
+          {/* Text */}
+          <h1 className="text-3xl text-center md:text-justify font-bold">Cesar A. Quesada</h1>
+          <h2 className="text-xl text-center md:text-justify font-medium">Fullstack developer with a focus on frontend development</h2>
+          <p className="text-base text-center md:text-justify text-gray-700">Bayamon, Puerto Rico</p>
+
+          {/* Links */}
+          <div className="flex gap-2 md:justify-start justify-center mt-2 ">
+          <Link className="text-3xl hover:text-[#0077B5]" href={"https://www.linkedin.com/in/cesaraquesada/"}>
+            <FaLinkedin />
+          </Link>
+          <Link className="text-3xl hover:text-[#333]" href={"https://github.com/xocesar"}>
+            <FaGithubSquare />
+          </Link>
+          <Link className="text-3xl hover:text-[#E4405F]" href={"https://www.instagram.com/xocesarr/"}>
+            <FaSquareInstagram />
+          </Link>
+          <Link className="text-3xl hover:text-black" href={"https://x.com/xocesal"}>
+            <RiTwitterXFill />
+          </Link>
+          </div>
+        </div>
+        
+
+
+    </div>
+  )
+}
+
+export default PageHeader;
