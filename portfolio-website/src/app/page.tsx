eb6fc35e8afbcaab0cb3d3e55e73b8158b3af941
Cesar Quesada <cquesada1611@gmail.com>
2024-11-17 13:49:06 -0400

    Added below sections for future code

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -3,12 +3,19 @@ import PageHeader from "@/components/Header/index";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-[#eedccd] text-[#3b580c]">
-      {/* Centered Content */}
-      
-        <PageHeader />
+    <div>
+      <div className="flex flex-col justify-center items-center min-h-screen bg-[#eedccd] text-[#3b580c]">
+        {/* here goes the header */}
         
-      
+          <PageHeader />
+          
+        
+      </div>
+      <div className="flex flex-col items-center bg-[#eedccd] text-[#3b580c]">
+        {/* The other sections go here */}
+        
+      </div>
     </div>
+    
   );
 }
