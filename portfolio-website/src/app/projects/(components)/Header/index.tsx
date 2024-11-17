033d57093c3d1a9a735a698ff7af9cf60a169bc0
Cesar Quesada <cquesada1611@gmail.com>
2024-11-17 14:28:55 -0400

    fixed apos error

--- portfolio-website/src/app/projects/(components)/Header/index.tsx
+++ portfolio-website/src/app/projects/(components)/Header/index.tsx
@@ -5,7 +5,7 @@ function ProjectsHeader() {
     <div className="flex flex-col items-start mb-8 max-w-4xl mx-auto px-4">
       <h1 className="text-4xl font-extrabold mb-4">My Projects</h1>
       <p className="text-xl">
-        A collection of projects I've built using various technologies and frameworks.
+        A collection of projects I&apos;ve built using various technologies and frameworks.
       </p>
     </div>
   );
