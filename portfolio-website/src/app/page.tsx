4e4681650bbe512d1407e3c1be69bbb456c96980
Cesar Quesada <cquesada1611@gmail.com>
2024-12-11 13:34:26 -0400

    eliminated header to debug

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PageHeader from "@/components/header/index";
 import ProjectGrid from "@/components/projects";
 import AboutMe from "@/components/about-me";
 
@@ -7,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <div className="bg-[#eedccd] text-[#3b580c]">
-        <PageHeader />
+      
       </div>
       <div className="flex flex-col items-center bg-[#eedccd] text-[#3b580c]">
         {/* The other sections go here */}
