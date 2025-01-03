ce5ff55e447fa92e7f7558647982fe159ab802a6
Cesar Quesada <cquesada1611@gmail.com>
2025-01-03 16:14:51 -0400

    fixed apos mistake

--- portfolio-website/src/app/about/(components)/header/index.tsx
+++ portfolio-website/src/app/about/(components)/header/index.tsx
@@ -10,8 +10,8 @@ export default function Header() {
         
         <div className="flex flex-col pt-4 px-0 md:px-4 max-w-lg">
             <h1 className="font-extrabold text-4xl">About Me</h1>
-            <p className="text-lg pt-4 text-gray-800 text-justify">
-            I am a dedicated full-stack developer based in Puerto Rico, passionate about creating innovative web applications. I specialize in both frontend and backend development, using modern tools and frameworks to build intuitive user interfaces and robust server systems. As a quick learner, I'm always eager to take on new challenges and expand my technical expertise. I thrive in collaborative environments and am committed to delivering high-quality solutions while continuously growing as a developer.
+            <p className="text-lg pt-4 text-gray-700 text-justify">
+            I am a dedicated full-stack developer based in Puerto Rico, passionate about creating innovative web applications. I specialize in both frontend and backend development, using modern tools and frameworks to build intuitive user interfaces and robust server systems. As a quick learner, I&apos;m always eager to take on new challenges and expand my technical expertise. I thrive in collaborative environments and am committed to delivering high-quality solutions while continuously growing as a developer.
               </p>
         </div>
         
