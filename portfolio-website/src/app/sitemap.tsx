67149153971ac583c7c7b76aebf332bce0575dbe
Cesar Quesada <cquesada1611@gmail.com>
2025-03-30 08:43:19 -0400

    adding sitemap

---
+++ portfolio-website/src/app/sitemap.tsx
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://cesarquesada.dev',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    
+  ]
+}
